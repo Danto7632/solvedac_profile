d163006c284a5c4df70765cef32e085ff5f66d5c
김인성 <74861101+Danto7632@users.noreply.github.com>
2025-03-27 21:06:50 +0900

    Add files via upload

---
+++ api/proxy.js
@@ -0,0 +1,15 @@
+export default async function handler(req, res) {
+    const { url } = req.query;
+    if (!url) return res.status(400).send("Missing url");
+    
+    try {
+      const response = await fetch(url);
+      const data = await response.text();
+  
+      res.setHeader("Access-Control-Allow-Origin", "*");
+      res.status(200).send(data);
+    } catch (e) {
+      res.status(500).send("Error fetching URL");
+    }
+  }
+  
